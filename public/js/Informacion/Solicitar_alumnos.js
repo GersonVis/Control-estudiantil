b308b0a8cb4ca63f2260fa96978ebc14170bce13
GersonVis <visoso126@gmail.com>
2022-10-23 13:52:07 -0500

    iniciando ver persona

--- public/js/Informacion/Solicitar_alumnos.js
+++ public/js/Informacion/Solicitar_alumnos.js
@@ -5,6 +5,7 @@ solicitar_alumnos=(url, contenedor_respuesta)=>{
        if(respuesta){
                 personas=respuesta.contenido.map(datos => {
                 let interfaz_persona=alumno_lista(datos)
+                add_eventos_persona(interfaz_persona.principal)
                 contenedor_respuesta.appendChild(interfaz_persona.principal)
                 return interfaz_persona
             });
@@ -14,3 +15,9 @@ solicitar_alumnos=(url, contenedor_respuesta)=>{
 window.addEventListener("load", function(ev){
     solicitar_alumnos(url_alumnos, lista_contenedor_alumnos)//url_alumnos se encuentra en el index
 })
+
+function add_eventos_persona({principal}){
+    principal.addEventListener("click", function(){
+        mostrar_modal({id_modal: "modal_datos_persona"})
+    })
+}
